a4ab207aa430ff4229a2e93aa939c5f2c30e9a22
RuturajPatond <ruturajpatond20@gmail.com>
2024-10-10 19:43:14 +0530

    added comment for searchbox

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -7,7 +7,7 @@ export default function SearchBox({updateInfo}) {
     const API_URL = "https://api.openweathermap.org/data/2.5/weather";
     const API_KEY = "0fa33a73488c054889ce93fd05522298";
     let [city, setCity] = useState("");
-
+    // searchbox to search city
     let getWeatherInfo = async () => {
         let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
         let jsonResponse = await response.json();
